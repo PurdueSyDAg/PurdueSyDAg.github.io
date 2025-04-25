ba27a8a0a160d7245ce144d72a6b02fe49413d30
cisdeli <pedrohcisdeli@gmail.com>
2025-04-25 16:47:33 -0400

    chore: better responsiveness and font sizes

--- SymposiumWebSite/hero.py
+++ SymposiumWebSite/hero.py
@@ -3,30 +3,60 @@ import reflex as rx
 
 def hero_section() -> rx.Component:
     return rx.box(
-        rx.vstack(
-            rx.heading(
-                "1st Annual Symposium of Digital Agriculture (SyDAG)",
-                size="7",
-                text_align="center",
+        rx.desktop_only(
+            rx.vstack(
+                rx.heading(
+                    "1st Annual Symposium of Digital Agriculture (SyDAG)",
+                    size="9",
+                    text_align="center",
+                ),
+                rx.text(
+                    "From Innovation to Impact",
+                    size="9",
+                    text_align="center",
+                ),
+                rx.link(
+                    rx.button("Register Now"),
+                    href="#register",
+                    is_external=False,
+                ),
+                spacing="5",
+                justify="center",
+                align_items="center",
+                height="90vh",
+                width="100%",
             ),
-            rx.text(
-                "From Innovation to Impact",
-                size="8",
-                text_align="center",
-            ),
-            rx.link(
-                rx.button("Register Now"),
-                href="#register",
-                is_external=False,
-            ),
-            spacing="5",
-            justify="center",
-            align_items="center",
-            height="90vh",
+            id="home",
             width="100%",
+            class_name="hero",
+            padding_y="2em",
         ),
-        id="home",
-        width="100%",
-        class_name="hero",
-        padding_y="2em",
+        rx.mobile_only(
+            rx.vstack(
+                rx.heading(
+                    "1st Annual Symposium of Digital Agriculture (SyDAG)",
+                    size="6",
+                    text_align="center",
+                ),
+                rx.text(
+                    "From Innovation to Impact",
+                    size="5",
+                    text_align="center",
+                ),
+                rx.link(
+                    rx.button("Register Now"),
+                    href="#register",
+                    is_external=False,
+                ),
+                spacing="5",
+                justify="center",
+                align_items="center",
+                height="90vh",
+                width="100%",
+            ),
+            id="home",
+            width="100%",
+            class_name="hero",
+            padding_y="2em",
+        ),
     )
